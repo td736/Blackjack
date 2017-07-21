4ae99ae8fe10f07f513e97129641b602a034f028
Unknown <td736@york.ac.uk>
2017-07-21 23:54:14 +0100

    Deck class - incomplete.

--- Dealer.py
+++ Dealer.py
@@ -1,9 +1,19 @@
-# from Deck import Deck
+from Deck import Deck
 
 class Dealer:
 
     def __init__(self):
         self.cards = []
+        self.deck = Deck()
+
+    def deal_to_player(self, player):
+
+        if len(player.cards) == 0:
+            player.cards.append(self.deck.draw_card())
+            player.cards.append(self.deck.draw_card())
+        else:
+            player.cards.append(self.deck.draw_card())
+
 
     def player_bust(self, player):
 
---
+++ Deck.py
@@ -0,0 +1,5 @@
+class Deck:
+
+    def __init__(self):
+        self.card_values = {'2': 2, '3': 3, '4': 4,
+            '10': 10, 'J': 11, 'Q': 11, 'K': 11, 'A': 11}
