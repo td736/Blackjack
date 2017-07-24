8afd8e1ac8f94be815e494b6f1dd051e7e512238
Unknown <td736@york.ac.uk>
2017-07-24 15:30:55 +0100

    Updated playere

--- Player.py
+++ Player.py
@@ -1,12 +1,26 @@
 class Player:
 
-    def __init__(self, money=1000):
+    def __init__(self, money=1000, name='Null'):
         self.money = money
         self.cards = []
-        self.total = 0
+        self.card_value = 0
+        self.name = name
+        self.money_bet = 0
 
-    def add_money(self, amount):
-        self.money += amount
+    def receive_money(self):
+        pass
 
-    def sub_money(self, amount):
+    def bet(self, amount):
+        amount = int(amount)
+        while amount > self.money:
+            amount = int(input("You only have ${}. \nBet amount: ".format(self.money)))
         self.money -= amount
+        self.money_bet = amount
+
+    def reset_hand(self):
+        self.cards = []
+        self.card_value = 0
+        self.money_bet = 0
+
+    def display_cards(self):
+        print ('{} at {}'.format(self.cards, self.card_value))
